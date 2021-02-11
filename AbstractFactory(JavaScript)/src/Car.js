2e9d107726e745cba582e3c85113e1c851c3d41d
fapaccha <43886181+fapaccha@users.noreply.github.com>
2021-02-11 09:52:42 -0500

    Add files via upload

---
+++ AbstractFactory(JavaScript)/src/Car.js
@@ -0,0 +1,28 @@
+var Vehicle = require('./Vehicle.js');
+
+class CarType1 extends Vehicle {
+  constructor() {
+    super("car", "super");
+  }
+}
+
+class CarType2 extends Vehicle {
+  constructor() {
+    super("car", "deportivo");
+  }
+}
+
+class CarType3 extends Vehicle {
+  constructor() {
+    super("car", "combi");
+  }
+}
+
+class CarType4 extends Vehicle {
+  constructor() {
+    super("car", "clasico");
+  }
+}
+
+export{CarType1,CarType2,CarType3,CarType4};
+
